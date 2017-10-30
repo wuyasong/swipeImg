8cd297ad64458e7b07cc48dd8137426d83569190
wuyasong <371758073@qq.com>
2017-10-30 22:48:22 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # swipeImg
 手机端swipe组件
+
+> 版本0.1：
+	支持图片滑动功能，大量absolute，translate3d属性未优化
+> 版本0.2：
+	支持图片滑动功能，优化大量absolute，translate3d引起的内存消耗问题
+> 版本0.3：
+	支持图片滑动功能，优化大量absolute，translate3d引起的内存消耗问题，支持图片异步加载功能
+
+使用说明：
+
+    new Swipe({
+        dom: document.querySelector('#canvas'),
+        list: document.querySelectorAll('#canvas li'),
+        lazy: 'data-src'
+    });
+实例化方法保存在js文件的全局对象里
+
+参数：
+* 接收一个对象3个属性
+* dom:  图片列表的外包DOM元素（必填）
+* list: 图片列表DOM元素集合（必填）
+* lazy: 图片异步加载的自定义属性名称（选填）
---
+++ swipeImg-0.2.js
@@ -0,0 +1,189 @@
+/*
+ * 图片滑动组件
+ * 适用于移动设备仅WebKit内核浏览器
+ * 支持IOS,Android,不支持win
+ * version: 0.2
+ * 优化图片定位对设备内存电量消耗
+ **/
+(function(window, document){
+	function Swipe(options){
+		this.wrap = options.dom;
+		this.list = options.list;
+
+		this.init();
+		this.renderDOM();
+		this.bindDOM();
+	}
+
+	Swipe.prototype = {
+		//初始化
+		init: function(){
+			//设定窗口比例
+			this.radio = window.innerHeight / window.innerWidth;
+			//设定一页的宽度
+			this.scaleW = window.innerWidth;
+			//设定初始索引值
+			this.idx = 0;
+		},
+		//渲染页面
+		renderDOM: function(){
+			var data = this.list;
+			var len = data.length;
+			var width = this.scaleW;
+			//设定li的宽和位置
+			for (var i = 0; i < data.length; i++) {
+				//前两个 显示 并 绝对定位
+				if (i < 3) {
+					data[i].style.display = '-webkit-box';
+					data[i].style.position = 'absolute';
+					data[i].style.width = width + 'px';
+					data[i].style.webkitTransform = 'translate3d(' + i * width + 'px, 0, 0)';
+					continue;
+				}
+				//其他都隐藏
+				data[i].style.width = width + 'px';
+				data[i].style.display = 'none';
+			}
+
+			//this.wrap.style.width = width + 'px';
+			this.wrap.style.height = window.innerHeight + 'px';
+		},
+		//绑定DOM事件
+		bindDOM: function(){
+			//
+			var elem = this.wrap;
+			var self = this;
+			var scaleW = self.scaleW;
+
+			function startHandle(e){
+				var data = this.list;
+				//记录起始时间
+				self.startTime = +new Date();
+				//记录按下坐标
+				self.startX = e.touches[0].pageX;
+				//清除位移值
+				self.offsetX = 0;
+
+			}
+			function moveHandle(e){
+				e.preventDefault();
+				//计算位移值
+				self.offsetX = e.touches[0].pageX - self.startX;
+				//计算上一张索引
+				var i = self.idx - 1;
+				//计算下一张索引
+				var n = self.idx + 1;
+				//获取DOM元素
+				var list = self.list;
+				
+				//改变DOM位移及过渡时间属性
+				for (i; i <= n; i++) {
+					list[i] && (list[i].style.webkitTransition = '');  //防止下次滑动产生延迟
+					list[i] && (list[i].style.webkitTransform = 'translate3d(' + ((i - self.idx) * scaleW + self.offsetX) + 'px, 0, 0)');
+					
+				}
+			}
+			function endHandle(e){
+				e.preventDefault();
+				//定义滑动临界值
+				var limX = self.scaleW / 2;
+				//定义快速滑动临界值
+				var fastLimX = 10;
+				//记录时间
+				var endTime = +new Date();
+				//当时间大于定义值
+				if (endTime - self.startTime > 300) {
+					//当位移大于临界值
+					if (self.offsetX >= limX) {
+						self.go(-1);  //上一页
+					} else if (self.offsetX < 0 && self.offsetX < -limX) {
+						self.go(1);   //下一页
+					} else {
+						self.go(0);   //本页
+					}
+				} else {
+					//当位移大于临界值
+					if (self.offsetX >= fastLimX) {
+						self.go(-1);  //上一页
+					} else if (self.offsetX < -fastLimX) {
+						self.go(1);   //下一页
+					} else {
+						self.go(0);   //本页
+					}
+				}
+
+			}
+			//绑定触摸事件
+			elem.addEventListener('touchstart', startHandle);
+			elem.addEventListener('touchmove', moveHandle);
+			elem.addEventListener('touchend', endHandle);
+		},
+		go: function(n){
+			var elem = this.list;
+			var index = this.idx;
+			var len = elem.length;
+			var c = n;
+			//当前滑动索引
+			n = n + index;
+			//判断极值
+			if (n < 0) {
+				n = 0;
+			}
+			if (n > len - 1) {
+				n = len - 1;
+			}
+			//保存索引值
+			this.idx = n;
+			//向后看
+			if (c > 0 && n < len - 1) {
+				if (elem[n-2]) {
+					elem[n-2].style.display = 'none';
+					elem[n-2].style.position = '';
+					elem[n-2].style.webkitTransform = '';
+				}
+				elem[n+1] && (elem[n+1].style.display = '-webkit-box');
+				elem[n+1] && (elem[n+1].style.position = 'absolute');
+			}
+			//向前看
+			if (c < 0 && n > 0) {
+				if (elem[n+2]) {
+					elem[n+2].style.display = 'none';
+					elem[n+2].style.position = '';
+					elem[n+2].style.webkitTransform = '';
+				}
+				elem[n-1] && (elem[n-1].style.display = '-webkit-box');
+				elem[n-1] && (elem[n-1].style.position = 'absolute');
+			}
+
+			//上述位移动画执行结束后回调
+			elem[n].addEventListener('webkitTransitionEnd', clearTransition, false);
+
+			//改变过渡时间
+			elem[n].style.webkitTransition = '-webkit-transform 0.2s ease-out';
+			//改变位移
+			elem[n].style.webkitTransform = 'translate3d(0, 0, 0)';
+			//前一张添加过渡动画
+			if (elem[n-1]) {
+				elem[n-1].style.webkitTransition = '-webkit-transform 0.2s ease-out';
+				elem[n-1].style.webkitTransform = 'translate3d(-' + this.scaleW + 'px, 0, 0)';
+				elem[n-1].addEventListener('webkitTransitionEnd', clearTransition, false);
+			}
+			//后一张添加过渡动画
+			if (elem[n+1]) {
+				elem[n+1].style.webkitTransition = '-webkit-transform 0.2s ease-out';
+				elem[n+1].style.webkitTransform = 'translate3d(' + this.scaleW + 'px, 0, 0)';
+				elem[n+1].addEventListener('webkitTransitionEnd', clearTransition, false);
+			}
+
+			function clearTransition(){
+				this.style.webkitTransition = '';
+			}
+		}
+	}
+	
+	//实例化组件
+	new Swipe({
+		dom: document.querySelector('#canvas'),
+		list: document.querySelectorAll('#canvas li')
+	});
+})(window, document);
